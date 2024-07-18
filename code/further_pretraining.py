d248cdb9130815d5378880b093eec5351650893d
quim-motger <quim.motger@gmail.com>
2024-07-18 11:21:53 +0200

    refactor + add instance selection + add extended pretraining

---
+++ code/further_pretraining.py
@@ -0,0 +1,145 @@
+import argparse, os
+
+from CoNLL_reader import convert_reviews_to_dict
+from CoNLL_reader import read_doc
+
+from transformers import XLNetTokenizer, AutoTokenizer, AutoModelForMaskedLM, AutoModelForCausalLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+import torch
+from torch.utils.data import Dataset
+from datasets import Dataset, DatasetDict
+from sklearn.model_selection import train_test_split
+
+
+## WANDB set up
+import wandb
+wandb.login()
+
+wandb.init(project="extended-pretraining")
+##
+
+def convert_dict_to_set(dict):
+    data_set = []
+    for i, key in enumerate(dict.keys()):
+        item = {'id': i, 'ner_tags': [], 'tokens': []}
+        for word_line in dict[key]['word-lines']:
+            if len(word_line) > 1:
+                if word_line[10].strip() == 'O':
+                    item['ner_tags'].append(0)
+                elif word_line[10].strip() == 'B-feature':
+                    item['ner_tags'].append(1)
+                elif word_line[10].strip() == 'I-feature':
+                    item['ner_tags'].append(2)
+                item['tokens'].append(word_line[2])
+        data_set.append(item)
+    return data_set
+
+block_size = 128
+
+def group_texts(examples):
+
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+def preprocess_function(data):
+    return tokenizer([" ".join(x) for x in data["tokens"]])
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-f', '--file', required=True, help="File containing the raw data")
+ap.add_argument('-o', '--output', required=True, help="Output folder to save checkpoints")
+ap.add_argument('-b', '--batch-size', required=True, help="Batch size", type=int, default=16)
+ap.add_argument('-e', '--epochs', required=True, help="Epochs", type=int, default=10)
+ap.add_argument('-m', '--model', required=True, help="Model")
+
+args = vars(ap.parse_args())
+
+file_path = args['file']
+output_path = args['output']
+batch_size = args['batch_size']
+epochs = args['epochs']
+model_name = args['model']
+wandb.run.name = model_name
+
+# Step 1: Load data
+set_file = read_doc(file_path)
+set_dict = convert_reviews_to_dict(set_file)
+set = convert_dict_to_set(set_dict)
+
+train_set, eval_set = train_test_split(set, test_size=0.01, random_state=42)
+
+train_set_d = Dataset.from_list(train_set)
+eval_set_d = Dataset.from_list(eval_set)
+data_set = DatasetDict({'train': train_set_d, 'test': train_set_d})
+
+# Step 2: Tokenize the Corpus
+if 'xlnet' in model_name:
+    tokenizer = XLNetTokenizer.from_pretrained("xlnet-large-cased")
+    print("Causal LM tokenizer: " + model_name)
+else:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    print("MLM tokenizer: " + model_name)
+
+tokenized_reviews = data_set.map(
+    preprocess_function,
+    batched=True,
+    num_proc=4,
+    remove_columns=data_set['train'].column_names,
+)
+
+dataset = tokenized_reviews.map(group_texts, batched=True, num_proc=4)
+
+if 'xlnet' in model_name:
+    tokenizer.pad_token = tokenizer.eos_token
+
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+if 'xlnet' in model_name:
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    print("Causal LM model: " + model_name)
+else:
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    print("MLM model: " + model_name)
+
+training_args = TrainingArguments(
+    output_dir=output_path,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    logging_strategy="steps",
+    learning_rate=3e-05,
+    weight_decay=0.01,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=epochs,
+    load_best_model_at_end=True,
+    push_to_hub=False,
+    logging_dir=os.path.join(output_path, "logs"),
+    logging_steps=500,
+    #eval_steps=100,
+    #max_steps=100,
+    #save_steps=100,
+    report_to="wandb"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"],
+    data_collator=data_collator,
+)
+
+trainer.train()
+
+import math
+
+eval_results = trainer.evaluate()
+print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
---
+++ code/instance_selection.py
@@ -0,0 +1,123 @@
+import argparse
+import os
+import json
+import random
+import numpy as np
+
+from CoNLL_reader import convert_dict_to_conll, convert_reviews_to_dict, read_doc
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import BertTokenizer, BertModel
+import torch
+
+# Categories
+categories = ['PRODUCTIVITY', 'COMMUNICATION', 'TOOLS', 'SOCIAL', 'HEALTH_AND_FITNESS',
+              'PERSONALIZATION', 'TRAVEL_AND_LOCAL', 'MAPS_AND_NAVIGATION', 'LIFESTYLE', 'WEATHER']
+
+# Bins
+bins = ['bin0', 'bin1', 'bin2', 'bin3', 'bin4', 'bin5', 'bin6', 'bin7', 'bin8', 'bin9']
+
+# Instances
+data_splits = [0.125, 0.25, 0.50, 0.75]
+
+# Arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-if', '--input-folder', required=True, help="Folder containing train and tests sets")
+ap.add_argument('-of', '--output-folder', required=True, help="Folder where checkpoints and results will be saved")
+ap.add_argument('-sf', '--sub-folders', required=True, help="Sub-folders (i.e., none, category, bin)")
+
+args = vars(ap.parse_args())
+input_folder = args['input_folder']
+output_folder = args['output_folder']
+sf = args['sub_folders']
+
+# Define the iteration of subfolders
+if sf == 'category':
+    subfolders = categories
+elif sf == 'bin':
+    subfolders = bins
+else:
+    subfolders = ['']
+
+# Specify the path to your JSON file
+json_file_path = os.path.join("data", "feature-review-wo-game.json")
+metadata = []
+
+# Load BERT model and tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+def get_bert_embeddings(texts):
+    inputs = tokenizer(texts, return_tensors='pt', padding=True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    embeddings = outputs.last_hidden_state.mean(dim=1)
+    return embeddings
+
+def calculate_similarity(X):
+    similarity_matrix = cosine_similarity(X)
+    np.fill_diagonal(similarity_matrix, 0)  # Set diagonal elements to 0 to avoid self-similarity
+    return similarity_matrix
+
+for data_split in data_splits:
+    
+    split_metadata = []
+
+    # Open the JSON file for reading
+    with open(json_file_path, "r") as file:
+        # Load the JSON data into a Python dictionary
+        feature_reviews = json.load(file)
+        
+        for subfolder in subfolders:
+            
+            train_set_dict_new = {}
+            new_dict = {}
+                
+            print("Starting instance selection process for " + subfolder)
+
+            # Process reviews
+            print("Reading docs...")
+            train_set_file = read_doc(os.path.join(input_folder, subfolder, "train-set.txt"))
+
+            print("Done.\nFormatting docs...")
+            train_set_dict = convert_reviews_to_dict(train_set_file)
+            
+            for key, value in feature_reviews.items():
+                # Calculate the size of the array
+                # Iterate through train_set_dict and count occurrences of key
+                count = 0
+                for instance_key in train_set_dict:
+                    if instance_key in value:
+                        count += 1
+                # Initialize the 'current' property to 0
+                current = 0
+                # Create a dictionary entry with the key and the desired properties
+                new_dict[key] = {"total": count, "current": current}
+            
+            # Generate BERT embeddings for the texts
+            concatenated_texts = [' '.join(subarray[1] for subarray in review['word-lines'] if len(subarray) > 1) for review in train_set_dict.values()]
+            X_train_bert = get_bert_embeddings(concatenated_texts).numpy()
+
+            # Calculate similarity matrix between instances
+            similarity_matrix = calculate_similarity(X_train_bert)
+
+            # Calculate average similarity for each instance
+            average_similarity = np.mean(similarity_matrix, axis=1)
+
+            # Sort keys based on average similarity (lower similarity means higher priority)
+            sorted_keys = [key for _, key in sorted(zip(average_similarity, train_set_dict.keys()))]
+
+            # Filter instances
+            for review_id in sorted_keys:
+                review = train_set_dict[review_id]
+                for feature, reviews in feature_reviews.items():
+                    if review_id in reviews:
+                        if new_dict[feature]['current'] < new_dict[feature]['total']*data_split:
+                            train_set_dict_new[review_id] = review
+                            new_dict[feature]['current'] += 1
+                            
+            split_metadata.append((len(train_set_dict_new)))
+                                        
+            convert_dict_to_conll(train_set_dict_new, os.path.join(output_folder, subfolder, "train-set_" + str(data_split) + ".txt"))
+            
+    metadata.append(split_metadata)
+print(metadata)
